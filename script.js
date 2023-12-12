c2dc2d4ba32bb90a5b1e6369c67f337e9c040a29
DonConcha <150167557+DonConcha@users.noreply.github.com>
2023-12-11 22:38:24 -0600

    Update script.js
    added function

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordTextEl = document.querySelector('.passwordHolder');
+
+var lowerCaseLetters = 'abcdefghijkmnlopqrstuvwxyz';
+var lowerCaseArr = lowerCaseLetters.split('');
+var upperCaseArr = lowerCaseLetters.toUpperCase().split('');
+var numberArr = [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9];
+var specialChars = '!@#$%^&*{}[]';
+var specialArr = specialChars.split('');
+
+function generatePassword(){
+var finalCharsArr = [];
+var finalPasswordArr = [];
+
+var userChoicePasswordLength = prompt('How many characters?');
+var userChoicelower = confirm('Use lower case?');
+var userChoiceUpper = confirm('use upper case?');
+var userChoiceNumbers = confirm('use numbers?');
+var userChoiceSpecials = confirm('use special characters');
 
 // Write password to the #password input
 function writePassword() {
